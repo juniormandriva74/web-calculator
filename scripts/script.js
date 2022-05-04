e7e3f8d7ae6e7b896afcd4548f1896950253e43b
Junior Santos <junior.mandriva74@gmail.com>
2022-05-03 21:15:40 -0300

    Add functions for updateDisplay and clear

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,52 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operator]');
-const equalsButtons = document.querySelector('[data-equals]');
-const deleteButtons = document.querySelector('[data-delete]');
-const allClearButtons = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButtons = document.querySelector("[data-equals]");
+const deleteButtons = document.querySelector("[data-delete]");
+const allClearButtons = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
 
 class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement;
-        this.currentOperandTextElement = currentOperandTextElement;
-    }
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
 
+  /*
+  appendNumber(number) {
+      this.currentOperand = `${this.currentOperand}${number.toString()}`;
+  }*/
+
+  updateDisplay() {
+    this.previousOperandTextElement.innerText = this.previousOperand;
+    this.currentOperandTextElement.innerText = this.currentOperand;
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 }
+
+const calculator = new Calculator(
+  previousOperandTextElement,
+  currentOperandTextElement
+);
+
+/*
+for (const numberButton for numberButtons) {
+    numberButton.addEventListener("click", () => {
+        calculator.appendNumber(numberButton.innerText);
+        calculator.updateDisplay();
+    });
+}*/
+
+allClearButtons.addEventListener("click", () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
